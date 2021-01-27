9a2624166d5ebe13e449e279775c5ed3eb3ee037
Helen Li <helen82198@gmail.com>
2021-01-26 17:10:46 -0800

    optimize get annotations to also get replies

--- routes/annotations.js
+++ routes/annotations.js
@@ -9,6 +9,7 @@ const HtmlLocation = require('../models').HtmlLocation;
 const Tag = require('../models').Tag;
 const router = express.Router();
 const { Op } = require("sequelize");
+const utils = require('../models/utils')(require('../models'));
 
 
 /**
@@ -135,69 +136,68 @@ router.get('/annotation', (req, res)=> {
         required: true,
         include:[
           {association: 'HeadAnnotation', attributes:['id', 'content', 'visibility', 'anonymity', 'created_at'],
-          include:[
-            {association: 'Author', attributes: ['id', 'first_name', 'last_name', 'username']},
-            {association: 'ReplyRequesters', attributes: ['id', 'first_name', 'last_name', 'username']},
-            {association: 'Starrers', attributes: ['id', 'first_name', 'last_name', 'username']},
-            {association: 'TaggedUsers', attributes: ['id']},
-            {association: 'Tags', attributes: ['tag_type_id']},
-            {association: 'Bookmarkers', attributes: ['id']}
-          ]},
+            include:[
+              {association: 'Author', attributes: ['id', 'first_name', 'last_name', 'username']},
+              {association: 'ReplyRequesters', attributes: ['id', 'first_name', 'last_name', 'username']},
+              {association: 'Starrers', attributes: ['id', 'first_name', 'last_name', 'username']},
+              {association: 'TaggedUsers', attributes: ['id']},
+              {association: 'Tags', attributes: ['tag_type_id']},
+              {association: 'Bookmarkers', attributes: ['id']}
+            ]
+          },
+          {association: 'AllAnnotations', separate: true, attributes:['id', 'content', 'visibility', 'anonymity', 'created_at'],
+            include:[
+              {association: 'Parent', attributes: ['id']},
+              {association: 'Author', attributes: ['id', 'first_name', 'last_name', 'username']},
+              {association: 'ReplyRequesters', attributes: ['id', 'first_name', 'last_name', 'username']},
+              {association: 'Starrers', attributes: ['id', 'first_name', 'last_name', 'username']},
+              {association: 'TaggedUsers', attributes: ['id']},
+              {association: 'Tags', attributes: ['tag_type_id']},
+              {association: 'Bookmarkers', attributes: ['id']}
+            ]
+          },
           {association: 'SeenUsers', attributes: ['id', 'first_name', 'last_name', 'username']},
           {association: 'RepliedUsers', attributes: ['id', 'first_name', 'last_name', 'username']},
         ]}
       ]})
-      .then(locations => {        
-        let annotations = locations
-          .filter((location) => {
-            let head = location.Thread.HeadAnnotation;
-            if (head.visibility === 'MYSELF'
-              && head.Author.id !== req.session.userId) {
+      .then(locations => {      
+        let annotations = {}
+        let headAnnotations = []
+
+        // TODO: is this the correct way to filter replies?
+        let goodLocations = locations.filter((location) => {
+          let head = location.Thread.HeadAnnotation;
+          if (head.visibility === 'MYSELF'
+            && head.Author.id !== req.session.userId) {
+            return false;
+          }
+          if (head.visibility === 'INSTRUCTORS' && !isUserInstructor) {
+            return false;
+          } if (isUserStudent && head.Author.id !== req.session.userId && !usersICanSee.has(head.Author.id) && !instructors.has(head.Author.id)) {
               return false;
+          }
+          return true;
+        })
+        
+        goodLocations.forEach((location) => {
+          // store all head annotaitons
+          headAnnotations.push(utils.createAnnotation(location, location.Thread.HeadAnnotation, instructors, req.session.userId))
+          
+          // store all associated annotations in {parent_id : annotation} annotations object
+          location.Thread.AllAnnotations.forEach((annotation) => {
+            if (annotation.Parent) {
+              if (!(annotation.Parent.id in annotations)) {
+                annotations[annotation.Parent.id] = []
+              }
+              if (location.Thread.AllAnnotations.length > 1) {
+                console.log(location.Thread.AllAnnotations.length);
+              }
+              annotations[annotation.Parent.id].push(utils.createAnnotation(location, annotation, instructors, req.session.userId ))
             }
-            if (head.visibility === 'INSTRUCTORS' && !isUserInstructor) {
-              return false;
-            } if (isUserStudent && head.Author.id !== req.session.userId && !usersICanSee.has(head.Author.id) && !instructors.has(head.Author.id)) {
-                return false;
-            }
-            return true;
           })
-          .map((location) => {
-            let annotation = {};
+        });
 
-            let range = location.HtmlLocation;
-            let head = location.Thread.HeadAnnotation;
-
-            annotation.id = head.id;
-            annotation.range = {
-              start: range.start_node,
-              end: range.end_node,
-              startOffset: range.start_offset,
-              endOffset: range.end_offset
-            };
-            annotation.parent = null;
-            annotation.timestamp = head.dataValues.created_at;
-            annotation.author = head.Author.id;
-            annotation.authorName = head.Author.first_name + " " + head.Author.last_name;
-            annotation.instructor = instructors.has(head.Author.id);
-            annotation.html = head.content;
-            annotation.hashtags = head.Tags.map(tag => tag.tag_type_id);
-            annotation.people = head.TaggedUsers.map(userTag => userTag.id);
-            annotation.visibility = head.visibility;
-            annotation.anonymity = head.anonymity;
-            annotation.replyRequestedByMe = head.ReplyRequesters
-              .reduce((bool, user)=> bool || user.id == req.session.userId, false);
-            annotation.replyRequestCount = head.ReplyRequesters.length;
-            annotation.starredByMe = head.Starrers
-              .reduce((bool, user)=> bool || user.id == req.session.userId, false);
-            annotation.starCount = head.Starrers.length;
-            annotation.seenByMe = location.Thread.SeenUsers
-              .reduce((bool, user)=> bool || user.id == req.session.userId, false);
-            annotation.bookmarked = head.Bookmarkers
-              .reduce((bool, user)=> bool || user.id == req.session.userId, false);
-            return annotation;
-          });
-        res.status(200).json(annotations);
+        res.status(200).json({'headAnnotations': headAnnotations, 'annotationsData': annotations});
 
       })
   });
@@ -353,11 +353,9 @@ router.get('/reply/:id', (req, res)=> {
 
 });
 
-
-
 /**
  * Make new reply for a given annotation
- * @name GET/api/annotations/reply/:id
+ * @name POST/api/annotations/reply/:id
  * @param id: id of parent annotation
  * @param content: text content of annotation
  * @param author: id of author
